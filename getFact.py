30a3ff6837a7c5e08f5af769650c96ae54be29dd
Aidan <aidanb04@uw.edu>
2025-02-25 13:13:10 -0800

    fact getter

---
+++ getFact.py
@@ -0,0 +1,17 @@
+import requests
+
+def get_random_wikipedia_fact():
+    # Get a random article title
+    url = "https://en.wikipedia.org/api/rest_v1/page/random/summary"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        title = data["title"]
+        summary = data["extract"]
+        return f"{title}: {summary}"
+    else:
+        return "Couldn't fetch a random Wikipedia fact."
+
+print(get_random_wikipedia_fact())
+
